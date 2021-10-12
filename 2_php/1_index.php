b4c378376db26a3362252c78f557cf0399645e32
zskgit <zskgit@gmail.com>
2021-10-12 07:20:53 +0200

    teb

--- 2_php/1_index.php
+++ 2_php/1_index.php
@@ -6,5 +6,39 @@
   </head>
   <body>
     <p>Strona główna kurs</p>
+    <?php
+      $name='Janusz';
+      $surname="Nowak";
+      echo 'Imię: $name<br>'; //Imię: $name
+      echo "Imię: $name<hr>"; //Imię: Janusz
+      // echo "Imię i nazwisko: $name $surname<br>Kurs programowania<br>";
+
+// heredoc
+  echo <<< LABEL
+    Imię i nazwisko: $name $surname<br>
+    Kurs programowania
+    <br>
+LABEL;
+
+  $text = <<< LABEL
+    <hr>
+    Opcja II <br>
+    Imię i nazwisko: $name $surname<br>
+    Kurs programowania
+    <br>
+LABEL;
+
+echo $text;
+
+// składnia nowdoc
+echo <<< 'LABEL'
+  <br>
+  Imię i nazwisko: $name $surname<br>
+  Kurs programowania
+  <br>
+LABEL;
+
+
+     ?>
   </body>
 </html>
